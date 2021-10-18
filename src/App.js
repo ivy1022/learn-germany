6ec00c0f238729195cae33ddec6e26ee47832334
ivy1022 <yoonhak.x.aa@gmail.com>
2021-10-18 23:02:52 +0200

    handleclick

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import React from 'react';
 class Phrase extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {phrase: 'Ja!'};
+    this.state = 'start!';
+  }
+
+  handleClick() {
+    this.setState({});
   }
   
   render() { 
-    const germans = ['ja', 'danke', 'thus'];
+    const germans = ['ja', 'word', 'danke'];
     const oneGerman = germans[Math.floor( Math.random() * germans.length)];
     return (
       <div className="phrase-box">
         <h2>Today's German phrase</h2>
         <h1>{ oneGerman }</h1>
         <button onClick = {() => {
-          // this.handleClick
+          this.handleClick();
         }}> next</button>
       </div>
   );
