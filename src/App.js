01adc7615e0a24ac78b6887b96981ef70c6d224f
ivy1022 <yoonhak.x.aa@gmail.com>
2021-10-18 19:50:57 +0200

    added array of germans

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 
 class Phrase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {phrase: 'Ja!'};
+  }
+  
   render() { 
+    const germans = ['ja', 'danke', 'thus'];
+    const oneGerman = germans[Math.floor( Math.random() * germans.length)];
     return (
       <div className="phrase-box">
-        <h1>Today's German phrase</h1>
+        <h2>Today's German phrase</h2>
+        <h1>{ oneGerman }</h1>
+        <button onClick = {() => {
+          // this.handleClick
+        }}> next</button>
       </div>
   );
   }
